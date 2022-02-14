bac4ab3c233e524fabae003f0d0eaa4160a6ebba
FernandoNoleto <fernando.barroso.noleto@gmail.com>
2022-02-14 17:44:52 -0300

    Adiciona player do youtube, porém falta arrumar o funcionamento

--- lib/pages/detailed_exercise_page.dart
+++ lib/pages/detailed_exercise_page.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+/*dependencies*/
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// import 'firebase_options.dart';
+
+
+
+
+
+
 class DetailedExercisePage extends StatefulWidget {
   const DetailedExercisePage({Key? key}) : super(key: key);
 
@@ -8,13 +22,79 @@ class DetailedExercisePage extends StatefulWidget {
 }
 
 class _DetailedExercisePageState extends State<DetailedExercisePage> {
+  //@override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     floatingActionButton: null,
+  //     body: StreamBuilder(
+  //         stream: Firestore.instance.collection('Videos').snapshots(),
+  //         builder:
+  //             (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+  //           if (!snapshot.hasData) {
+  //             return const Center(
+  //               child: CircularProgressIndicator(),
+  //             );
+  //           }
+  //
+  //           return ListView(
+  //             children: snapshot.data.documents.map((document) {
+  //               var url = document['url'];
+  //
+  //               YoutubePlayerController _controller = YoutubePlayerController(
+  //                 initialVideoId: YoutubePlayer.convertUrlToId(url)!,
+  //                 flags: const YoutubePlayerFlags(
+  //                   autoPlay: false,
+  //                   mute: false,
+  //                   disableDragSeek: false,
+  //                   loop: false,
+  //                   isLive: false,
+  //                   forceHD: false,
+  //                 ),
+  //               );
+  //
+  //               return Center(
+  //                 child: SizedBox(
+  //                   width: MediaQuery.of(context).size.width / 1.2,
+  //                   child: Column(
+  //                     children: <Widget>[
+  //                       Padding(
+  //                         padding: const EdgeInsets.only(top: 20, bottom: 5,),
+  //                         child: Text(document['title']),
+  //                       ),
+  //                       YoutubePlayer(
+  //                         controller: _controller,
+  //                         liveUIColor: Colors.amber,
+  //                       ),
+  //                     ],
+  //                   ),
+  //                 ),
+  //               );
+  //             }).toList(),
+  //           );
+  //         }),
+  //   );
+  // }
+
+  static String myVideoId = 'hwf01VTWMCo';
+
+  // Initiate the Youtube player controller
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: myVideoId,
+    flags: const YoutubePlayerFlags(
+      autoPlay: true,
+      mute: false,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Exercicio detalhado"),
-      ),
-      body: const Text("Exercicio detalhado"),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter and Youtube'),
+        ),
+        body: YoutubePlayer(
+          controller: _controller,
+          liveUIColor: Colors.amber,
+        ));
   }
 }
